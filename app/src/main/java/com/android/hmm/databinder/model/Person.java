85f252dbd378ca77d43b52010d196b447336f0e3
houmaomian <houmaomian@gmail.com>
2015-08-03 14:36:50 +0800

    use BaseObservable

--- app/src/main/java/com/android/hmm/databinder/model/Person.java
+++ app/src/main/java/com/android/hmm/databinder/model/Person.java
@@ -1,13 +1,41 @@
 package com.android.hmm.databinder.model;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import com.android.hmm.databinder.BR;
+
 /**
  * Created by hmm on 2015/8/3.
  */
-public class Person {
+public class Person extends BaseObservable{
 
-    public String name;
+    private String name;
 
-    public int age;
+    private int age;
 
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Bindable
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        notifyPropertyChanged(BR.name);
+    }
+
+    @Bindable
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+        notifyPropertyChanged(BR.age);
+    }
 
 }
--- app/src/main/java/com/android/hmm/databinder/ui/MainActivity.java
+++ app/src/main/java/com/android/hmm/databinder/ui/MainActivity.java
@@ -3,8 +3,13 @@ package com.android.hmm.databinder.ui;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 import com.android.hmm.databinder.R;
 import com.android.hmm.databinder.databinding.ActivityMainBinding;
 import com.android.hmm.databinder.model.Person;
@@ -12,14 +17,25 @@ import com.android.hmm.databinder.model.Person;
 
 public class MainActivity extends ActionBarActivity {
 
+    @InjectView(R.id.edit_name) EditText editName;
+
+    private Person mPerson;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        Person person = new Person();
-        person.name = "hou";
-        person.age = 21;
-        binding.setPerson(person);
+        ButterKnife.inject(this);
+        mPerson= new Person("test",8888);
+        binding.setPerson(mPerson);
+    }
+
+    @OnClick(R.id.bt_change)
+    public void changeName(){
+        String text = editName.getText().toString();
+        if(TextUtils.isEmpty(text))
+            return;
+        mPerson.setName(text);
     }
 
     @Override
